6975edc191c9d4a6cd13286c9b73c5eb9feb7d48
Zuhaib Imtiaz <zuhaib.imtiaz.ios@gmail.com>
2025-03-05 13:13:21 +0400

    initial comit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZBAppRouter",
+    platforms: [
+        .iOS(.v16),
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ZBAppRouter",
+            targets: ["ZBAppRouter"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "ZBAppRouter",
+            path: "./Sources/ZBAppRouterFramework.xcframework"
+        )
+
+    ]
+)
